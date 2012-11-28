f2b6484731a95e9ae5f39e0952ccd0c4d8634da3
hoang <xmen6290@yahoo.com>
2012-11-28 10:13:37 +0800

    Update layout

--- js/helper/desktop.js
+++ js/helper/desktop.js
@@ -9,23 +9,301 @@ define(['helper/app','libs/text!template/desktop.tpl'],function(App,templ){
 
 	App.DesktopController = Em.Controller.extend({
 		initDate: true,
+		currentPosition: 0,
+		currentViewWidth: 0,
 
 		initDateList: function(){
 			var self = this;
 
 			var settings = {	// example of full settings
 				width: '100%',					// Width (in %) of slider compared to window width
-				height: '45px',					// Height (in px) of slider
+				height: '61px',					// Height (in px) of slider
 				arrows: true,					// Toggle left & right arrows (optional, default false)
-				arrowsWidth: '5%',				// Width (in %) of left & right arrows compared to slider (optional, default 8%)
-				mainWidth: '90%',				// Width (in %) of main view compared to slider (optional, default 80% if arrows enabled and 100% otherwise)
+				arrowsWidth: '10%',				// Width (in %) of left & right arrows compared to slider (optional, default 8%)
+				mainWidth: '80%',				// Width (in %) of main view compared to slider (optional, default 80% if arrows enabled and 100% otherwise)
 				mainMargin: '0',				// Margin (in %) of main view compared to slider (optional, default 0 2% if arrows enabled and 0 otherwise)
 				imgPath: 'img',					// Path to img folder (where arrow_left.png & arrow_right.png present)
-				itemWidth: '13%'				// Width (in %) of each item compared to main view or absolute (in px)
+				itemWidth: '11%'				// Width (in %) of each item compared to main view or absolute (in px)
 			};
 			if (self.initDate){
 				$('#espntv_select_date').mySlider(settings);
+				self.currentViewWidth = $('#espntv_select_date').width();
+				self.bindDateList();
+				self.bindTimeList();
+				self.bindProgramList();
 				self.initDate = false;
+			}
+		},
+
+		bindDateList: function(){
+			var self = this;
+			var $dateList = $('#espntv_select_date ul li');
+			$dateList.on('mySlider_click_event',function(){
+				$dateList.find('img').prependTo(this);
+				$dateList.removeClass('espntv_selected_date');
+				$(this).addClass('espntv_selected_date');
+			});
+		},
+
+		bindTimeList: function(){
+			var self = this;
+			var $timeList = $('#espntv_time_line ul');
+			var $programList = $('#espntv_programs ul');
+
+			$timeList.parent().find('.espntv_time_arrow_left').on('mousedown touchstart',function(ev){
+				ev.preventDefault();
+				self.currentPosition += self.currentViewWidth;
+				if (self.currentPosition > 0){
+					self.currentPosition = 0;
+				}
+				$timeList.animate({
+					left: self.currentPosition
+				},300);
+				$programList.animate({
+					left: self.currentPosition
+				},300);
+			});
+
+			$timeList.parent().find('.espntv_time_arrow_right').on('mousedown touchstart',function(ev){
+				ev.preventDefault();
+				self.currentPosition -= self.currentViewWidth;
+				if (self.currentPosition < -self.currentViewWidth*9.1){
+					self.currentPosition = -self.currentViewWidth*9.1;
+				}
+				$timeList.animate({
+					left: self.currentPosition
+				},300);
+				$programList.animate({
+					left: self.currentPosition
+				},300);
+			});
+
+			var time_line = document.getElementById('espntv_time_list');
+			var touch,startTouch,endLeft;
+			var init = false, dragging = false;
+
+			/* Listen to drag events */
+			
+			if (!time_line.addEventListener){ // if IE < 9
+				$timeList.parent().on('mousedown',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					startTouch = ev.screenX;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+				$timeList.parent().on('mousemove',function(ev){
+					ev.preventDefault();
+					if (init){
+						dragging = true;
+						endLeft = self.currentPosition + ev.screenX - startTouch;
+						if (endLeft > 0){
+							endLeft = 0;
+						}
+						if (endLeft < -self.currentViewWidth*9.1){
+							endLeft = -self.currentViewWidth*9.1;
+						}
+						$timeList.css('left',endLeft+'px');
+						$programList.css('left',endLeft+'px');
+					}
+				});
+				$timeList.parent().on('mouseup',function(ev){
+					ev.preventDefault();
+					init = false;
+				});
+			}
+			else{	// if IE9 or above
+				// Listen to touch events
+
+				time_line.addEventListener('touchstart',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+
+				time_line.addEventListener('touchmove',function(ev){
+					ev.preventDefault();
+					touch = ev.touches[0];
+					if (init){
+						dragging = true;
+						startTouch = touch.pageX;
+						init = false;
+					}
+					endLeft = self.currentPosition + touch.pageX - startTouch;
+					if (endLeft > 0){
+						endLeft = 0;
+					}
+					if (endLeft < -self.currentViewWidth*9.1){
+						endLeft = -self.currentViewWidth*9.1;
+					}
+					$timeList.css('left',endLeft+'px');
+					$programList.css('left',endLeft+'px');
+				});
+				
+				// time_line.addEventListener('touchend',function(ev){
+				// 	ev.preventDefault();
+				// 	if (dragging){
+				// 		$list.animate({
+				// 			left: '+='+(endLeft - info.currentLeft)/5+'px'
+				// 		},400,function(){
+				// 			adjustPosition($list,false);	
+				// 		});
+				// 	}
+				// });
+
+				// Listen to mouse events
+
+				time_line.addEventListener('mousedown',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					startTouch = ev.pageX;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+				time_line.addEventListener('mousemove',function(ev){
+					ev.preventDefault();
+					if (init){
+						dragging = true;
+						endLeft = self.currentPosition + ev.pageX - startTouch;
+						if (endLeft > 0){
+							endLeft = 0;
+						}
+						if (endLeft < -self.currentViewWidth*9.1){
+							endLeft = -self.currentViewWidth*9.1;
+						}
+						$timeList.css('left',endLeft+'px');
+						$programList.css('left',endLeft+'px');
+					}
+				});
+				time_line.addEventListener('mouseup',function(ev){
+					ev.preventDefault();
+					init = false;
+				});
+			}
+		},
+
+		bindProgramList: function(){
+			var programs = document.getElementById('espntv_programs');
+			var touch,startTouch,endLeft;
+			var init = false, dragging = false;
+			var $timeList = $('#espntv_time_line ul');
+			var $programList = $('#espntv_programs ul');
+
+			/* Listen to drag events */
+			
+			if (!programs.addEventListener){ // if IE < 9
+				$programList.parent().on('mousedown',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					startTouch = ev.screenX;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+				$programList.parent().on('mousemove',function(ev){
+					ev.preventDefault();
+					if (init){
+						dragging = true;
+						endLeft = self.currentPosition + ev.screenX - startTouch;
+						if (endLeft > 0){
+							endLeft = 0;
+						}
+						if (endLeft < -self.currentViewWidth*9.1){
+							endLeft = -self.currentViewWidth*9.1;
+						}
+						$timeList.css('left',endLeft+'px');
+						$programList.css('left',endLeft+'px');
+					}
+				});
+				$programList.parent().on('mouseup',function(ev){
+					ev.preventDefault();
+					init = false;
+				});
+			}
+			else{	// if IE9 or above
+				// Listen to touch events
+
+				programs.addEventListener('touchstart',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+
+				programs.addEventListener('touchmove',function(ev){
+					ev.preventDefault();
+					touch = ev.touches[0];
+					if (init){
+						dragging = true;
+						startTouch = touch.pageX;
+						init = false;
+					}
+					endLeft = self.currentPosition + touch.pageX - startTouch;
+					if (endLeft > 0){
+						endLeft = 0;
+					}
+					if (endLeft < -self.currentViewWidth*9.1){
+						endLeft = -self.currentViewWidth*9.1;
+					}
+					$timeList.css('left',endLeft+'px');
+					$programList.css('left',endLeft+'px');
+				});
+				
+				// Listen to mouse events
+
+				programs.addEventListener('mousedown',function(ev){
+					ev.preventDefault();
+					init = true;
+					dragging = false;
+					startTouch = ev.pageX;
+					self.currentPosition = parseFloat($timeList.css('left'));
+				});
+				programs.addEventListener('mousemove',function(ev){
+					ev.preventDefault();
+					if (init){
+						dragging = true;
+						endLeft = self.currentPosition + ev.pageX - startTouch;
+						if (endLeft > 0){
+							endLeft = 0;
+						}
+						if (endLeft < -self.currentViewWidth*9.1){
+							endLeft = -self.currentViewWidth*9.1;
+						}
+						$timeList.css('left',endLeft+'px');
+						$programList.css('left',endLeft+'px');
+					}
+				});
+				programs.addEventListener('mouseup',function(ev){
+					ev.preventDefault();
+					init = false;
+				});
+			}
+
+			$programList.find('li').on('mouseup touchend',function(ev){
+				ev.preventDefault();
+				if (!dragging){
+					//$(this).trigger('mySlider_click_event');
+					console.log($programList.find('li').index(this));
+				}
+			});
+		},
+
+		updateView: function(quick){
+			var self = this;
+			var $timeList = $('#espntv_time_line ul');
+			var $programList = $('#espntv_programs ul');
+
+			if (quick){
+				$timeList.css('left',self.currentPosition);
+				$programList.css('left',self.currentPosition);
+			}
+			else{
+				$timeList.animate({
+					left: self.currentPosition
+				},300);
+				$programList.animate({
+					left: self.currentPosition
+				},300);
 			}
 		}
 	});
--- js/template/desktop.tpl
+++ js/template/desktop.tpl
@@ -68,18 +68,108 @@
 		<div class="espntv_right_column">
 			<div id="espntv_select_date">
 				<ul>
-					<li><img src="img/date.png"><p>WED 3 Oct</p></li>
-					<li><img src="img/date.png"><p>THU 4 Oct</p></li>
-					<li><img src="img/date.png"><p>FRI 5 Oct</p></li>
-					<li><img src="img/date.png"><p>SAT 6 Oct</p></li>
-					<li><img src="img/date.png"><p>SUN 7 Oct</p></li>
-					<li><img src="img/date.png"><p>MON 8 Oct</p></li>
-					<li><img src="img/date.png"><p>TUE 9 Oct</p></li>
-					<li><img src="img/date.png"><p>WED 10 Oct</p></li>
-					<li><img src="img/date.png"><p>THU 11 Oct</p></li>
-					<li><img src="img/date.png"><p>FRI 12 Oct</p></li>
+					<li class="espntv_selected_date"><img src="img/date_selected.png"><p>WED 3 Oct</p></li>
+					<li><p>THU 4 Oct</p></li>
+					<li><p>FRI 5 Oct</p></li>
+					<li><p>SAT 6 Oct</p></li>
+					<li><p>SUN 7 Oct</p></li>
+					<li><p>MON 8 Oct</p></li>
+					<li><p>TUE 9 Oct</p></li>
+					<li><p>WED 10 Oct</p></li>
+					<li><p>THU 11 Oct</p></li>
+					<li><p>FRI 12 Oct</p></li>
+				</ul>
+
+			</div>
+			<div id="espntv_time_line">
+				<ul id="espntv_time_list">
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+					<li><img src="img/time_point.png"><p>Test</p></li>
+				</ul>
+				<div class="espntv_time_arrow_left"><img src="img/time_arrow_left.png"></div>
+				<div class="espntv_time_arrow_right"><img src="img/time_arrow_right.png"></div>
+			</div>
+			<div id="espntv_programs">
+				<ul>
+					<li class="espntv_live">
+						<p class="espntv_normal">SPORTS NEWS</p>
+						<div>
+							<p class="espntv_bold">Sportscenter Asia</p>
+							<img src="img/live_indicator.png">
+						</div>
+						<p class="espntv_normal">Monday Night Verdict</p>
+					</li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+				</ul>
+				<ul>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+				</ul>
+				<ul>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+				</ul>
+				<ul>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+				</ul>
+				<ul>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
+				</ul>
+				<ul>
+					<li><p>Test</p></li>
+					<li><p>Test</p></li>
 				</ul>
 			</div>
+			<div id="espntv_program_details">
+				<div class="espntv_details_bg"></div>
+				<div class="espntv_details">
+					<img class="espntv_details_channel" src="img/logo/espn.png">
+					<div class="espntv_details_content">
+						<p class="espntv_normal">FOOTBALL</p>
+						<p class="espntv_normal">WED 3 Oct</p>
+						<p class="espntv_normal">3:00 PM - 7:00 PM</p>
+						<p class="espntv_bold">Baltimore Orioles vs. Tampa Bay Rays</p>
+						<p class="espntv_normal">MLB Regular Season 2012</p>
+						<p class="espntv_replay">Replay</p>
+						<p class="espntv_normal">4 Oct 03:30 AM</p>
+						<p class="espntv_normal">9 Oct 03:30 PM</p>
+						<p class="espntv_normal">10 Oct 03:30 AM</p>
+						<img class="espntv_details_live" src="img/live_indicator.png">
+					</div>
+					<div class="espntv_details_share">
+						<p>SHARE</p>
+					</div>
+					<a class="espntv_details_close"><img src="img/details_close.png"></a>
+				</div>
+			</div>
 		</div>
+	</div>
+	<div class="espntv_bottom">
+		<img src="img/bottom_img.png">
+		<img class="espntv_bottom_live_icon" src="img/live_icon.png">
 	</div>
 </div>
